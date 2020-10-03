53c287abae087c88257365dd95fe023a78f42f13
Secco <louizoliveira03@hotmail.com>
2020-10-03 09:32:46 -0300

    feat: Criado componente de entidade.

--- src/app/pages/entidades/entidades.component.ts
+++ src/app/pages/entidades/entidades.component.ts
@@ -1,3 +1,4 @@
+import { NzModalService } from 'ng-zorro-antd';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -8,11 +9,68 @@ import { Router } from '@angular/router';
   styleUrls: ['./entidades.component.css'],
 })
 export class EntidadesComponent implements OnInit {
-  public validateForm!: FormGroup;
+  dataFilter = [];
+  data = [
+    {
+      nome_fantasia: 'Entidade',
+      razao_social: 'Entidade',
+      documento: 'Entidade',
+      localizacao: 'Entidade',
+    },
+    {
+      nome_fantasia: 'Entidade 2',
+      razao_social: 'Entidade 2',
+      documento: 'Entidade 2',
+      localizacao: 'Entidade 2',
+    },
+    {
+      nome_fantasia: 'Entidade 2',
+      razao_social: 'Entidade 2',
+      documento: 'Entidade 2',
+      localizacao: 'Entidade 2',
+    },
+    {
+      nome_fantasia: 'Entidade 2',
+      razao_social: 'Entidade 2',
+      documento: 'Entidade 2',
+      localizacao: 'Entidade 2',
+    },
+    {
+      nome_fantasia: 'Entidade 2',
+      razao_social: 'Entidade 2',
+      documento: 'Entidade 2',
+      localizacao: 'Entidade 2',
+    },
+    {
+      nome_fantasia: 'Entidade 2',
+      razao_social: 'Entidade 2',
+      documento: 'Entidade 2',
+      localizacao: 'Entidade 2',
+    },
+    {
+      nome_fantasia: 'Entidade 2',
+      razao_social: 'Entidade 2',
+      documento: 'Entidade 2',
+      localizacao: 'Entidade 2',
+    },
+    {
+      nome_fantasia: 'Entidade 2',
+      razao_social: 'Entidade 2',
+      documento: 'Entidade 2',
+      localizacao: 'Entidade 2',
+    },
+  ];
+  editar = false;
 
-  constructor(private fb: FormBuilder, public router: Router) {}
+  /**
+   * Formulario para filtragem do menu
+   */
+  search: FormGroup;
+  isVisible = false;
 
-  ngOnInit(): void {
+  validateForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private modal: NzModalService) {
     this.validateForm = this.fb.group({
       nome_fantasia: [null, [Validators.required]],
       razao_social: [null, [Validators.required]],
@@ -21,10 +79,67 @@ export class EntidadesComponent implements OnInit {
     });
   }
 
-  submitForm(): void {
-    for (const i of Object.keys(this.validateForm.controls)) {
-      this.validateForm.controls[i].markAsDirty();
-      this.validateForm.controls[i].updateValueAndValidity();
+  ngOnInit(): void {
+    this.search = this.fb.group({
+      search: '',
+    });
+    this.dataFilter = this.data;
+  }
+
+  pesquisar(value) {
+    if (value.search === '') {
+      this.dataFilter = this.data;
+      return;
     }
+
+    this.dataFilter = this.data.filter(
+      (x) => this.unaccent(x)?.indexOf(this.unaccent(value.search)) > -1
+    );
+  }
+
+  unaccent(value) {
+    return value
+      ?.toLowerCase()
+      .normalize('NFD')
+      .replace(/[^a-zA-Zs]/g, '');
+  }
+
+  submitForm(value: { descricao: string }): void {
+    for (const key of Object.keys(this.validateForm.controls)) {
+      this.validateForm.controls[key].markAsDirty();
+      this.validateForm.controls[key].updateValueAndValidity();
+    }
+    console.log(value);
+  }
+
+  showModal(item?: any): void {
+    this.isVisible = true;
+    this.editar = item != null;
+    this.validateForm.setValue({
+      descricao: item || '',
+      id: null,
+    });
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+
+  delete(): void {
+    this.modal.confirm({
+      nzTitle: 'Atenção!',
+      nzContent: 'Tem certeza que deseja remover a entidade?',
+      nzOkText: 'Remover',
+      nzOkType: 'danger',
+      nzOnOk: () => console.log('OK'),
+      nzCancelText: 'Cancelar',
+      nzOnCancel: () => console.log('Cancel'),
+    });
   }
 }
