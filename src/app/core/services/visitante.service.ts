d97b5355f5632291e55fd2a037605dd3bcdfa737
Secco <louizoliveira03@hotmail.com>
2020-11-25 22:16:38 -0300

    feat: jack adora uma modal

---
+++ src/app/core/services/visitante.service.ts
@@ -0,0 +1,17 @@
+import { Observable } from 'rxjs';
+import { EndpointsConstants } from 'src/app/constants/endpoints.constants';
+import { VisitanteDTO } from 'src/app/models/payload/visitante/visitante.dto';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class VisitanteService {
+  constructor(private http: HttpClient) {}
+
+  salvar(visanteDTO: VisitanteDTO): Observable<void> {
+    return this.http.post<void>(
+      EndpointsConstants.VISITANTE.SALVAR,
+      visanteDTO
+    );
+  }
+}
